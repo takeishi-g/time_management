30f53fcdf0fa7b5c3be7ec22b5a3df63e9557b8b
takeishi-g <89860661+takeishi-g@users.noreply.github.com>
2023-10-08 20:57:21 +0900

    workerlist作成

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,8 +3,8 @@ import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
--- frontend/pages/workers/index.js
+++ frontend/pages/workers/index.js
@@ -1,33 +1,10 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+import WorkersList from '@/components/workersList'
 
 const Workers = () => {
-  const [workers, setWorkers] = useState([
-    {
-      id: 0,
-      name: '',
-    },
-  ])
-
-  useEffect(() => {
-    axios
-      .get('http://localhost/api/workers')
-      .then((response) => setWorkers(response.data))
-      .catch((error) => console.log(error))
-  }, [])
-
   return (
     <div className=''>
       <h1 className=' text-lg'>加工者一覧</h1>
-      <ul>
-        {workers.map((worker) => {
-          return (
-          <li key={worker.id}>
-            {worker.name}
-          </li>
-          )
-        })}
-      </ul>
+      <WorkersList />
     </div>
   )
 }
